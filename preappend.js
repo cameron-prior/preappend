6af5e101aab952069f1bd1f487d1f914b54c512d
Cameron Prior <cprior313@gmail.com>
2017-11-04 09:27:07 -0400

    init commit with working preappend

---
+++ preappend.js
@@ -0,0 +1,10 @@
+/*
+ * This isn't good please don't use this
+ * I used leftpad for reference because idk how to NPM
+ */
+'use strict';
+module.exports = preappend;
+
+function preappend(pre, append) {
+    return pre + append;
+}
